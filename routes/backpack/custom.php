159d3da0a0362c445a5eba991479af185605b834
PeterGabrielVE <31576642+PeterGabrielVE@users.noreply.github.com>
2025-04-28 23:40:51 -0400

    crud reserva y sala

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -16,6 +16,8 @@ Route::group([
     ),
     'namespace' => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    Route::crud('sala', 'SalaCrudController');
+    Route::crud('reserva', 'ReservaCrudController');
 }); // this should be the absolute last line of this file
 
 /**
