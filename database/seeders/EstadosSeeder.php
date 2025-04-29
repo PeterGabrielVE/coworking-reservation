117c362f8ba4ab937ec61ec58d5d38d2992e3373
PeterGabrielVE <31576642+PeterGabrielVE@users.noreply.github.com>
2025-04-29 07:31:00 -0400

    estados para reserva migracion  y seeder

---
+++ database/seeders/EstadosSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Estado;
+
+class EstadosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Estado::create(['nombre' => 'pendiente']);
+        Estado::create(['nombre' => 'aceptada']);
+        Estado::create(['nombre' => 'rechazada']);
+    }
+}
